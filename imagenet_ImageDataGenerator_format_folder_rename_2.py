168ad0edef61b8b048223e952e5b778717bb3dab
Yung-Yu Tsai <leo51leo51leo51@gmail.com>
2018-09-20 16:43:56 +0800

    update

---
+++ imagenet_ImageDataGenerator_format_folder_rename_2.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 17 16:54:46 2018
+
+@author: Yung-Yu Tsai
+
+imagenet validation set folder management script
+put the raw image into label named folder for keras ImageDataGenerator
+"""
+
+import os, sys, ast
+
+dir = ''
+
+class_mapping=dict()
+
+with open(dir+'../imagenet_class_mapping.txt','r') as file_class_mapping:
+    for line in file_class_mapping:
+        line_content=line.split()
+        class_mapping[line_content[1]]=[line_content[0],line_content[2]]
+    #class_mapping=ast.literal_eval(class_mapping_file.read())
+    
+with open(dir+'../imagenet_class_index.json','r') as file_class_index:
+    imagenet_class_index=ast.literal_eval(file_class_index.read())
+    
+index_convert=dict()
+for key in imagenet_class_index.keys():
+    index_convert[imagenet_class_index[key][0]]=[key,imagenet_class_index[key][1]]
+
+ground_truth=list()
+
+with open(dir+'../ILSVRC2012_validation_ground_truth.txt','r') as file_ground_truth:
+    for line in file_ground_truth:
+        ground_truth.append(int(line))
+        
+        
+pic_file_list=os.listdir(dir+'../ILSVRC2012_img_val')
+
+
+#%%
+# rename folder
+    
+class_folder_list=os.listdir(dir+'../imagenet_val_imagedatagenerator')
+count=0
+for key in class_mapping.keys():
+    os.system('rename %s %s' % (str(key)+'_'+class_mapping[key][1],index_convert[class_mapping[key][0]][0]+'_'+index_convert[class_mapping[key][0]][1]))
+    count=count+1
+    print('foleder renameed %d/%d'%(count,1000))
+    
